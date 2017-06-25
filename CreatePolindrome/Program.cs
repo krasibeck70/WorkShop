eb2a9b62c5e68555738ddfa8cfb3936f99781519
Krasimir Rusev <krasibeck70@gmail.com>
2017-06-25 14:08:36 +0300

    FirstCommit

---
+++ CreatePolindrome/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatePolindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(IsPolindrome(input));
+            Console.WriteLine(SmallPolindrome(input));
+        }
+
+        private static string SmallPolindrome(string input)
+        {
+            Dictionary<char, int> charactersCount = new Dictionary<char, int>();
+            char[] charcters = input.ToCharArray();
+            string firstCharcters = "";
+            string secondCharcters = "";
+            var removeNumber = ' ';
+            InsertInDictionary(input, charactersCount);
+            var countEven = charactersCount.Where(x => x.Value % 2 == 1).ToList();
+            if (countEven.Count() > 1)
+            {
+                return input;
+            }
+            else if (charcters.Length % 2 == 0)
+            {
+                return PolindromeIsLengthEven(charactersCount, ref firstCharcters, ref secondCharcters);
+            }
+            else
+            {
+                return PolindromeIsLengthIsOdd(charactersCount, ref firstCharcters, ref secondCharcters, ref removeNumber);
+            }
+        }
+
+        private static string PolindromeIsLengthIsOdd(Dictionary<char, int> charactersCount, ref string firstCharcters, ref string secondCharcters, ref char removeNumber)
+        {
+            foreach (var charcter in charactersCount)
+            {
+                if (charcter.Value % 2 == 1)
+                {
+                    removeNumber = charcter.Key;
+                }
+                else
+                {
+                    var count = charcter.Value / 2;
+                    firstCharcters += new string(charcter.Key, count);
+                    secondCharcters += new string(charcter.Key, count);
+                }
+            }
+            char[] chars = secondCharcters.ToCharArray();
+            Array.Reverse(chars);
+            return firstCharcters + removeNumber + new string(chars);
+        }
+
+        private static string PolindromeIsLengthEven(Dictionary<char, int> charactersCount, ref string firstCharcters, ref string secondCharcters)
+        {
+            foreach (var chararter in charactersCount.OrderBy(x => x.Key))
+            {
+                var count = chararter.Value / 2;
+                firstCharcters += new string(chararter.Key, count);
+                secondCharcters += new string(chararter.Key, count);
+            }
+            char[] chars = secondCharcters.ToCharArray();
+            Array.Reverse(chars);
+            return firstCharcters + new string(chars);
+        }
+
+        private static void InsertInDictionary(string input, Dictionary<char, int> charactersCount)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!charactersCount.ContainsKey(input[i]))
+                {
+                    charactersCount.Add(input[i], 1);
+                }
+                else
+                {
+                    charactersCount[input[i]] += 1;
+                }
+            }
+        }
+
+        private static bool IsPolindrome(string input)
+        {
+            int min = 0;
+            int max = input.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = input[min];
+                char b = input[max];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+    }
+}
---
+++ PhoneBook/PhoneBook.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    public class PhoneBook
+    {
+
+        private const string PathBook = "C:\\Users\\BecK\\Documents\\visual studio 2017\\Projects\\Musala\\Musala\\phoneBook.txt";
+        private const string PathBookCalls = "C:\\Users\\BecK\\Documents\\Visual Studio 2017\\Projects\\Musala\\Musala\\phoneBookCalls.txt";
+        private const string Pattern = "(\\+359|0|00359)(87|88|89)([2-9])(\\d{6})";
+        private Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+
+        public void Set(string name, string number)
+        {
+            Regex regex = new Regex(Pattern);
+            if (regex.IsMatch(number))
+            {
+                var match = regex.Match(number).Groups;
+                string phone = "+359" + match[2].Value + match[3].Value + match[4].Value;
+                if (!phoneBook.ContainsKey(name))
+                {
+                    phoneBook.Add(name, phone);
+                }
+                else
+                {
+                    phoneBook[name] = phone;
+                }
+            }
+        }
+        /// <summary>
+        /// Read file and set key and value in to dictionary 
+        /// </summary>
+        private void ReadPhoneBookByFile()
+        {
+            var file = File.ReadLines(PathBook).Where(x => !string.IsNullOrWhiteSpace(x));
+            if (file.Count() != 1)
+            {
+                foreach (string line in file)
+                {
+                    var name = line.Split()[0];
+                    var number = line.Split()[1];
+                    Set(name, number);
+                }
+            }
+        }
+        /// <summary>
+        /// Add name and phone to the text
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="phone"></param>
+        public void AddName(string name, string phone)
+        {
+            ReadPhoneBookByFile();
+            if (!phoneBook.ContainsKey(name))
+            {
+                Set(name, phone);
+                SavePhoneBook(name + " " + phone);
+                Console.WriteLine($"{name} added successfully");
+            }
+            else
+            {
+                Console.WriteLine($"{name} was alredy exist");
+            }
+        }
+        /// <summary>
+        /// Find contact by name and print in to console
+        /// </summary>
+        /// <param name="name"></param>
+        public void FindByName(string name)
+        {
+            ReadPhoneBookByFile();
+            PrintByName(phoneBook.FirstOrDefault(x => x.Key.ToLower() == name.ToLower()), name);
+        }
+        private static void SavePhoneBook(string nameAndPhone)
+        {
+            File.AppendAllText(PathBook, Environment.NewLine + nameAndPhone);
+        }
+
+        /// <summary>
+        /// Delete contact by name
+        /// </summary>
+        /// <param name="name"></param>
+        public void DeleteNameAndPhone(string name)
+        {
+            ReadPhoneBookByFile();
+            if (phoneBook.Remove(name))
+            {
+                DeleteFromFile(name);
+                Console.WriteLine($"{name} was deleted!");
+            }
+            else
+            {
+                Console.WriteLine($"Not found {name}");
+            }
+        }
+        /// <summary>
+        /// Delete contact by name for file
+        /// </summary>
+        /// <param name="name"></param>
+        private void DeleteFromFile(string name)
+        {
+            string[] lines = System.IO.File.ReadAllLines(PathBook);
+
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter(PathBook))
+            {
+                foreach (string line in lines)
+                {
+                    if (!line.Contains(name))
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Call by name, and save in to file
+        /// </summary>
+        /// <param name="name"></param>
+        public void Call(string name)
+        {
+            ReadPhoneBookByFile();
+            if (phoneBook.ContainsKey(name))
+            {
+                SaveCall(name);
+            }
+            else
+            {
+                Console.WriteLine($"Not found {name}");
+            }
+        }
+
+        private void SaveCall(string name)
+        {
+            List<string> lines = File.ReadAllLines(PathBookCalls).ToList();
+            var text = File.ReadAllText(PathBookCalls);
+            var updateRecentCall = new List<string>();
+            if (lines.Count() != 0)
+            {
+                WriteToFile(name, lines, text, updateRecentCall);
+            }
+            else
+            {
+                updateRecentCall.Add($"{phoneBook.First(x => x.Key == name).Key} - " + $"{phoneBook[name]} (1)");
+            }
+            WriteToFile(updateRecentCall);
+        }
+
+        private void WriteToFile(string name, List<string> lines, string text, List<string> newLines)
+        {
+            lines.ForEach(delegate (string s) { newLines.Add(s); });
+            if (text.Contains(name))
+            {
+                var currentLine = "";
+                newLines.ForEach(delegate (string s) { if (s.Contains(name)) currentLine = s; });
+                newLines.Remove(currentLine);
+                var tokens = currentLine.Split(new[] { '-', '(', ')' });
+                newLines.Add($"{phoneBook.First(x => x.Key == name).Key} - " + $"{phoneBook[name]} " + $"({int.Parse(tokens[2]) + 1 })");
+            }
+            else
+            {
+                newLines.Add($"{phoneBook.First(x => x.Key == name).Key} - " + $"{phoneBook[name]} (1)");
+            }
+        }
+
+        private static void WriteToFile(List<string> newLines)
+        {
+            using (var writer = new StreamWriter(PathBookCalls))
+            {
+                foreach (string line in newLines)
+                {
+                    writer.WriteLine(line);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Print phone book in to console
+        /// </summary>
+        public void PrintPhoneBook()
+        {
+            ReadPhoneBookByFile();
+            if (phoneBook.Count != 0)
+            {
+                foreach (var book in this.phoneBook.OrderBy(name => name.Key))
+                {
+                    Console.WriteLine($"{book.Key} - {book.Value}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("contacts not found ");
+            }
+        }
+        /// <summary>
+        /// Print recent call in to console
+        /// </summary>
+        public void PrintRecentCalls()
+        {
+            var fiveMostCalled = 5;
+            var file = File.ReadAllLines(PathBookCalls);
+            Dictionary<string, int> sorteDictionary = new Dictionary<string, int>();
+            foreach (var line in file)
+            {
+                var tokens = line.Split(new[] { '-', '(', ')' });
+                sorteDictionary.Add(tokens[0] + " - " + tokens[1], Int32.Parse(tokens[2]));
+            }
+            foreach (var names in sorteDictionary.OrderByDescending(x => x.Value).ThenBy(x => x.Key).Take(fiveMostCalled))
+            {
+                Console.WriteLine($"{names.Key} ({names.Value})");
+            }
+        }
+        /// <summary>
+        /// Print contact by name in to console
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="name"></param>
+        public void PrintByName(KeyValuePair<string, string> value, string name)
+        {
+            if (value.Key != null)
+            {
+                Console.WriteLine(value.Key + " - " + value.Value);
+            }
+            else
+            {
+                Console.WriteLine($"Not found {name}");
+            }
+
+        }
+    }
+}
